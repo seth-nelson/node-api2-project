6e21f20048f6278e54643c9753c36dd1f1752f80
Seth Nelson <seth.nelson@outlook.com>
2020-05-12 16:41:35 -0700

    setup skeleton for routes in the posts-router file

---
+++ posts/posts-router.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../data/db');
+
+router.post('/', (req, res) => {
+
+});
+
+router.post('/:id/comments', (req, res) => {
+
+});
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.get('/:id/comments', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+
+module.exports = router;
