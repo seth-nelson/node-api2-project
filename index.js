22f7c71524ddde38fb38844b9199a59ed12aa812
Seth Nelson <seth.nelson@outlook.com>
2020-05-12 16:04:30 -0700

    setup index.js to run the server

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-// Used to create and start the server
+// Used to create and start the server listening
 
+const server = require('./server.js');
+
+const PORT = 4000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
